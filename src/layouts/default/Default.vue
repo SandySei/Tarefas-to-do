3e9c6c90516347d829b486ae2e48774737291cb6
Sandy <sandyalves8@hotmail.com>
2023-06-20 10:03:50 -0300

    Alteração no código do template com alterações nas cores, bordas e elevação do card de login e cadastro

--- src/layouts/default/Default.vue
+++ src/layouts/default/Default.vue
@@ -1,11 +1,11 @@
 <template>
-  <v-app>
+  <v-app class="bg-grey-lighten-4">
     <v-container
       class="d-flex flex-column justify-center align-center text-center h-100"
     >
       <v-toolbar
         fixed-tabs
-        class="d-flex justify-space-evenly alising-center bg-deep-orange w-100"
+        class="d-flex justify-space-evenly alising-center bg-red-accent-4 w-100 elevation-20 rounded-t-xl"
       >
         <router-link to="/" class="w-100 h-100">
           <v-tab class="text-black w-100 h-100" value="Entrar"> Entar </v-tab>
@@ -17,13 +17,13 @@
         </router-link>
       </v-toolbar>
 
-      <v-card class="d-flex flex-row align-center bg-grey-darken-4 w-100 h-85">
+      <v-card class="d-flex flex-row align-center bg-grey-darken-4 w-100 h-85 elevation-20 rounded-b-xl">
         <v-img
           :width="60"
           aspect-ratio="3/4"
           src="@/assets/imagens/Home.jpg"
         ></v-img>
-        <v-main class="w-25">
+        <v-main class="w-25" >
           <router-view></router-view>
         </v-main>
       </v-card>
