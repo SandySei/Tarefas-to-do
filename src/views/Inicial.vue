2c06e52f69ee4588e03c237a3ab371d9d28f4e76
Sandy <sandyalves8@hotmail.com>
2023-06-23 10:30:09 -0300

    Adição de minhas tarefas apos página login

--- src/views/Inicial.vue
+++ src/views/Inicial.vue
@@ -1,14 +1,24 @@
-<template>oi</template>
+<template>
+  <v-card v-for="list in toDoListis" :key="list.id">
+    <v-card-title>{{ list.title }}</v-card-title>
+  </v-card>
+</template>
 
 <script>
 import { toDoListApiMixin } from "@/api/toDoList";
 export default {
   mixins: [toDoListApiMixin],
+  data() {
+    return {
+      toDoListis: [],
+    };
+  },
+
   methods: {
     async getLists() {
       try {
         const { data } = await this.list();
-        console.log(data);
+        this.toDoListis = data;
       } catch (err) {
         alert("Algo deu errado.");
       }
