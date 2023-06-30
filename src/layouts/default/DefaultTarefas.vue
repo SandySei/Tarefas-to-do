207f40dd27fb23fef442f5dca2e9f6423cb356d5
Sandy <sandyalves8@hotmail.com>
2023-06-30 13:14:19 -0300

    Alteração de cores e imagens

--- src/layouts/default/DefaultTarefas.vue
+++ src/layouts/default/DefaultTarefas.vue
@@ -8,10 +8,9 @@
       >
         <div class="h-100 d-flex flex-column">
           <v-list>
-            <v-img
-              height="100"
-              src="@/assets/Tarefas1.png"
-            ></v-img>
+            <router-link to="/inicial">
+              <v-img height="100" src="@/assets/TarefasMenu.png"></v-img>
+            </router-link>
           </v-list>
 
           <v-list density="compact" nav>
@@ -27,22 +26,29 @@
               to="/inicial"
             ></v-list-item>
 
+            <v-divider></v-divider>
+
             <v-list-item
-              prepend-icon="mdi-format-list-checks"
-              v-for="list in toDoListis"
+              prepend-icon="mdi-check-all"
+              v-for="list in sortedLists"
               :key="list.id"
               :to="`/viewItem/${list.id}`"
               :title="list.title"
             >
             </v-list-item>
+
+            <v-divider></v-divider>
           </v-list>
 
           <v-list class="d-flex align-end flex-grow-1" density="compact" nav>
             <v-list-item
-              class="bg-red-darken-4 w-100"
+              :class="{ 'bg-red-accent-4': isMouseOver }"
+              class="w-100"
               @click="logOut"
               prepend-icon="mdi-power"
               title="Sair"
+              @mouseover="handleMouseOver"
+              @mouseleave="handleMouseLeave"
             ></v-list-item>
           </v-list>
         </div>
@@ -51,11 +57,11 @@
       <v-main>
         <router-view
           @getLists="getLists"
-          :lists="toDoListis"
+          :lists="sortedLists"
           :selectedList="$route.params.id"
           @snackbar="sendSnackbarEvent"
         ></router-view>
-    </v-main>
+      </v-main>
     </v-layout>
   </v-card>
 </template>
@@ -67,15 +73,22 @@ export default {
   mixins: [toDoListApiMixin],
   data() {
     return {
-      toDoListis: [],
+      toDoLists: [],
+      isMouseOver: false,
     };
   },
-
+  computed: {
+    sortedLists() {
+      return this.toDoLists
+        .slice()
+        .sort((a, b) => a.title.localeCompare(b.title));
+    },
+  },
   methods: {
     async getLists() {
       try {
         const { data } = await this.list();
-        this.toDoListis = data;
+        this.toDoLists = data;
       } catch (err) {
         this.$emit("snackbar", "Algo deu errado!");
       }
@@ -92,9 +105,21 @@ export default {
     sendSnackbarEvent(message) {
       this.$emit("snackbar", message);
     },
+    handleMouseOver() {
+      this.isMouseOver = true;
+    },
+    handleMouseLeave() {
+      this.isMouseOver = false;
+    },
   },
   mounted() {
     this.getLists();
   },
 };
 </script>
+
+<style>
+main {
+  height: 100vh;
+}
+</style>
--- src/views/Inicial.vue
+++ src/views/Inicial.vue
@@ -1,35 +1,54 @@
 <template>
   <v-container class="w-75 d-flex flex-column justify-self-center">
-    <v-img height="100" src="@/assets/Tarefasextenso.png" class="pa-2"></v-img>
+    <div class="w-100 d-flex align-items-center flex-row">
+      <v-text-field
+        v-model="searchQuery"
+        prepend-inner-icon="mdi-magnify"
+        class="w-75 altura-x"
+        label="Procurar"
+        variant="solo-inverted"
+        :hide-details="true"
+      ></v-text-field>
 
-    <router-link to="/addItem" class="text-decoration-none">
-      <v-card prepend-icon="mdi-plus" class="ma-3 d-flex">
-        <template v-slot:title> Adicionar Lista de Tarefas </template>
-      </v-card>
-    </router-link>
+      <div class="w-25">
+        <router-link to="/addItem" class="text-decoration-none">
+          <v-card
+            prepend-icon="mdi-plus"
+            class="ml-3 d-flex altura-x add"
+            elevation="2"
+          >
+            <template v-slot:title>Lista de Tarefas </template>
+          </v-card>
+        </router-link>
+      </div>
+    </div>
 
-    <div class="bg-grey-lighten-2 pa-4 rounded-t-xl rounded-b-xl elevation-20 mb-5">
+    <v-img
+      height="100"
+      src="@/assets/TarefasInicial.png"
+      class="mt-5 mb-5"
+    ></v-img>
 
-      <v-text-field v-model="searchQuery" label="Procurar" outlined class="color- pl-5 pr-5"></v-text-field>
-      
+    <div
+      class="bg-grey-darken-4 pa-4 rounded-lg elevation-10 mb-5"
+    >
+      <v-card-title class="text-red-accent-4">Minhas Listas:</v-card-title>
       <div class="pl-2 pr-2">
-        
         <v-card
           v-for="list in filteredLists"
           :key="list.id"
-          variant="tonal"
-          class="ma-2 bg-grey-lighten-4"
+          class="my-2 bg-grey-darken-3"
         >
           <router-link
             :to="`/viewItem/${list.id}`"
             class="text-decoration-none d-flex align-center justify-space-between pl-15 pr-16"
           >
-            <v-card-title class="text-black">{{ list.title }}</v-card-title>
+            <v-card-title class="text-white">{{ list.title }}</v-card-title>
 
             <v-icon
               icon="mdi mdi-file-edit"
               size="large"
-              color="black"
+              color="white"
             ></v-icon>
           </router-link>
         </v-card>
@@ -53,7 +72,7 @@ export default {
   computed: {
     filteredLists() {
       if (this.searchQuery) {
-        return this.lists.filter(list =>
+        return this.lists.filter((list) =>
           list.title.toLowerCase().includes(this.searchQuery.toLowerCase())
         );
       }
@@ -65,3 +84,18 @@ export default {
   },
 };
 </script>
+
+<style>
+.altura-x {
+  height: 55px;
+}
+
+.altura-x .v-input__control {
+  height: 55px !important;
+}
+
+.add:active {
+  background-color: #424242;
+  color: white;
+}
+</style>
